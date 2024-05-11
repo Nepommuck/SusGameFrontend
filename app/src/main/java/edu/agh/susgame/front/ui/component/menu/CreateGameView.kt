a354581a3894b33a475f130f222b1f4b077ec4fb
uqsz <uqsz123@gmail.com>
2024-05-11 23:59:34 +0200

    textfield for game name and pin

--- app/src/main/java/edu/agh/susgame/front/ui/component/menu/CreateGameView.kt
+++ app/src/main/java/edu/agh/susgame/front/ui/component/menu/CreateGameView.kt
@@ -1,22 +1,43 @@
 package edu.agh.susgame.front.ui.component.menu
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.navigation.NavController
 import edu.agh.susgame.front.ui.Translation
 import edu.agh.susgame.front.ui.component.common.Header
 import edu.agh.susgame.front.ui.component.menu.navigation.MenuRoute
 import edu.agh.susgame.front.ui.theme.PaddingL
 
+private const val maxPinLength = 6
 @Composable
 fun CreateGameView(
     navController: NavController,
 ) {
+    var gameName by remember { mutableStateOf("") }
+    var gamePIN by remember { mutableStateOf("") }
+    var showPassword by remember { mutableStateOf(value = false) }
     Column(Modifier.padding(PaddingL)) {
         Column(
             Modifier
@@ -24,6 +45,50 @@ fun CreateGameView(
                 .weight(1f)
         ) {
             Header(title = Translation.Menu.CREATE_GAME)
+
+            OutlinedTextField(
+                label = { Text("Nazwa gry") },
+                value = gameName,
+                onValueChange = { gameName = it},
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true
+            )
+
+            Spacer(modifier = Modifier.width(PaddingL))
+            OutlinedTextField(
+                label = { Text("PIN gry") },
+                visualTransformation =  if (showPassword) {
+
+                    VisualTransformation.None
+
+                } else {
+
+                    PasswordVisualTransformation()
+
+                },
+                trailingIcon = {
+                    if (showPassword) {
+                        IconButton(onClick = { showPassword = false }) {
+                            Icon(
+                                imageVector = Icons.Filled.Visibility,
+                                contentDescription = "hide_password"
+                            )
+                        }
+                    } else {
+                        IconButton(
+                            onClick = { showPassword = true }) {
+                            Icon(
+                                imageVector = Icons.Filled.VisibilityOff,
+                                contentDescription = "hide_password"
+                            )
+                        }
+                    }
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                value = gamePIN,
+                onValueChange = { if (it.length <= maxPinLength) gamePIN = it},
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true  )
 
             // TODO GAME-52
             Text(text = "TODO GAME-52")
@@ -35,3 +100,4 @@ fun CreateGameView(
         }
     }
 }
+
