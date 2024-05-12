5aba5010d47a5134bc0d3b12f859bf76fa7bae6a
uqsz <uqsz123@gmail.com>
2024-05-12 12:51:29 +0200

    final commmit for 52

--- app/src/main/java/edu/agh/susgame/front/ui/Translation.kt
+++ app/src/main/java/edu/agh/susgame/front/ui/Translation.kt
@@ -8,6 +8,12 @@ object Translation {
         const val LEAVE = "Opuść"
         const val JOIN = "Dołącz"
         const val PLAY = "Graj"
+        const val CREATE = "Stwórz"
+    }
+
+    object Toasts {
+        const val CREATE_NO_GAME_NAME = "Podaj nazwę gry!"
+        const val CREATE_SUCCESS = "Pomyślnie stworzono grę!"
     }
 
     object Menu {
@@ -20,10 +26,15 @@ object Translation {
                 return "Oczekuje $n ${if (n == 1) "gracz" else "graczy"}"
             }
         }
-
         const val CREATE_GAME = "Stwórz nową grę"
+    }
+    object CreateGame{
         const val ENTER_GAME_NAME = "Nazwa gry"
         const val ENTER_GAME_PIN = "PIN gry"
+        const val AMOUNT_OF_PLAYERS = "Liczba graczy:"
+        const val GAME_TIME = "Czas gry:"
+        const val MINUTES = " minuty "
+        const val DEFAULT_GAME_NAME = "Uqsz's game"
     }
 
     object Game {
--- app/src/main/java/edu/agh/susgame/front/ui/component/menu/CreateGameView.kt
+++ app/src/main/java/edu/agh/susgame/front/ui/component/menu/CreateGameView.kt
@@ -1,10 +1,13 @@
 package edu.agh.susgame.front.ui.component.menu
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
@@ -21,29 +24,40 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+
 import androidx.navigation.NavController
 import edu.agh.susgame.front.ui.Translation
 import edu.agh.susgame.front.ui.component.common.Header
 import edu.agh.susgame.front.ui.component.menu.navigation.MenuRoute
 import edu.agh.susgame.front.ui.theme.PaddingL
 
+import java.util.Locale
+import kotlin.math.roundToInt
+
+
 private const val maxPinLength = 6
 private const val maxPlayersAmount = 6
 private const val minPlayersAmount = 2
+private const val defaultPlayersAmount = 4
+private const val minGameTime = 5f
+private const val maxGameTime = 15f
+private const val defaultGameTime = 10
 @Composable
 fun CreateGameView(
     navController: NavController,
 ) {
-    var gameName by remember { mutableStateOf("") }
+    var gameName by remember { mutableStateOf(Translation.CreateGame.DEFAULT_GAME_NAME) }
     var gamePIN by remember { mutableStateOf("") }
     var showPassword by remember { mutableStateOf(value = false) }
-    var selectedNumber by remember { mutableIntStateOf(2) }
-    var gameTime by remember { mutableStateOf(10) }
+    var selectedNumber by remember { mutableIntStateOf(defaultPlayersAmount) }
+    var gameTime by remember { mutableIntStateOf(defaultGameTime) }
     Column( Modifier.padding(PaddingL)) {
         Column(
             Modifier
@@ -54,17 +68,17 @@ fun CreateGameView(
 
             // entering game name
             OutlinedTextField(
-                label = { Text(Translation.Menu.ENTER_GAME_NAME) },
+                label = { Text(Translation.CreateGame.ENTER_GAME_NAME) },
                 value = gameName,
-                onValueChange = { gameName = it},
+                onValueChange = { gameName = it },
                 modifier = Modifier.fillMaxWidth(),
                 singleLine = true
             )
 
             // entering game pin
             OutlinedTextField(
-                label = { Text(Translation.Menu.ENTER_GAME_PIN) },
-                visualTransformation =  if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+                label = { Text(Translation.CreateGame.ENTER_GAME_PIN) },
+                visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
                 trailingIcon = {
                     if (showPassword) {
                         IconButton(onClick = { showPassword = false }) {
@@ -85,56 +99,84 @@ fun CreateGameView(
                 },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                 value = gamePIN,
-                onValueChange = { if (it.length <= maxPinLength) gamePIN = it},
+                onValueChange = { if (it.length <= maxPinLength) gamePIN = it },
                 modifier = Modifier.fillMaxWidth(),
                 singleLine = true
             )
-            Row(){
-                Column(verticalArrangement = Arrangement.SpaceEvenly){
-                    Text(
-                        text = "Liczba graczy:",
-                    )
-                    Text(
-                        text = "Czas gry:",
-                    )
-                }
-                Column(verticalArrangement = Arrangement.SpaceEvenly) {
-                    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly){
-                        Button(
-                            onClick = { selectedNumber -= 1 },
-                            enabled = selectedNumber > minPlayersAmount
-                        ) {
-                            Text(text = "-")
-                        }
-                        Text(
-                            text = "$selectedNumber",
-                            modifier = Modifier.padding(PaddingL),
-                            fontWeight = FontWeight.Bold
-                        )
-                        Button(
-                            onClick = { selectedNumber += 1 },
-                            enabled = selectedNumber < maxPlayersAmount
-                        ) {
-                            Text(text = "+")
-                        }
-                    }
-                    Slider(
-                        value = gameTime.toFloat(),
-                        onValueChange = { newValue ->
-                            gameTime = newValue.toInt()
-                        },
-                        valueRange = 1f..60f, // Zakres czasu gry od 1 do 60 minut
-                        steps = 59,
-                    )
 
+            // entering maximal amount of players
+            Row(
+                Modifier.padding(PaddingL).fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(
+                    text = Translation.CreateGame.AMOUNT_OF_PLAYERS
+                )
+                Spacer(modifier = Modifier.width(PaddingL))
+                Button(
+                    onClick = { selectedNumber -= 1 },
+                    enabled = selectedNumber > minPlayersAmount
+                ) {
+                    Text(text = "-")
+                }
+                Text(
+                    text = "$selectedNumber",
+                    modifier = Modifier.padding(PaddingL),
+                    fontWeight = FontWeight.Bold
+                )
+                Button(
+                    onClick = { selectedNumber += 1 },
+                    enabled = selectedNumber < maxPlayersAmount
+                ) {
+                    Text(text = "+")
                 }
             }
 
-
-
-
-            // TODO GAME-52
-            Text(text = "TODO GAME-52")
+            // entering game time
+            Row(
+                Modifier.padding(PaddingL).fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(
+                    modifier = Modifier.weight(0.2f),
+                    text = Translation.CreateGame.GAME_TIME
+                )
+                Text(
+                    text = String.format(
+                        Locale.getDefault(),
+                        "%02d",
+                        gameTime
+                    ) + Translation.CreateGame.MINUTES,
+                )
+                Spacer(modifier = Modifier.width(PaddingL))
+                Slider(
+                    modifier = Modifier.weight(0.3f),
+                    value = gameTime.toFloat(),
+                    onValueChange = { newValue ->
+                        gameTime = newValue.toInt()
+                    },
+                    valueRange = minGameTime..maxGameTime,
+                    steps = (maxGameTime- minGameTime-1).roundToInt()
+                )
+            }
+            // button to create new game
+            Row(
+                Modifier.padding(PaddingL).fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                val context = LocalContext.current
+                Button(onClick = {
+                    // TODO: stworzenie nowej gry z odpowiednimi parametrami
+                    if (gameName == "") Toast.makeText( context,Translation.Toasts.CREATE_NO_GAME_NAME, Toast.LENGTH_SHORT).show()
+                    else Toast.makeText( context,Translation.Toasts.CREATE_SUCCESS, Toast.LENGTH_SHORT).show()
+//                    navController.navigate(MenuRoute.MainMenu.route)
+                }) {
+                    Text(text = Translation.Button.CREATE)
+                }
+            }
         }
         Button(onClick = {
             navController.navigate(MenuRoute.MainMenu.route)
