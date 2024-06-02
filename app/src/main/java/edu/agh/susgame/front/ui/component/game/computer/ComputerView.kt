df0038d55d5b6ea3f081eaf6c0505087e1454aa6
Pawel Konopka <p.konopka@avsystem.com>
2024-06-02 12:16:35 +0200

    Socket cleanup

--- app/src/main/java/edu/agh/susgame/front/ui/component/game/computer/ComputerView.kt
+++ app/src/main/java/edu/agh/susgame/front/ui/component/game/computer/ComputerView.kt
@@ -1,44 +1,15 @@
 package edu.agh.susgame.front.ui.component.game.computer
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import edu.agh.susgame.front.model.game.LobbyId
-import edu.agh.susgame.front.providers.web.socket.SocketDemo
 import edu.agh.susgame.front.ui.Translation
-import kotlinx.coroutines.flow.MutableSharedFlow
+import edu.agh.susgame.front.ui.component.common.Header
 
 @Composable
-fun ComputerComponent(lobbyId: LobbyId) {
-    val userInputFlow = MutableSharedFlow<String>()
-    val serverResponseFlow = MutableSharedFlow<String>()
-
-    val chatHistory = mutableListOf<String>()
-
-    LaunchedEffect(serverResponseFlow) {
-//        CoroutineScope(Dispatchers.Main).launch {
-        serverResponseFlow.collect { message ->
-            chatHistory.add(message)
-//            }
-        }
-    }
-
-    // Unreachable code
-    SocketDemo(userInputFlow, serverResponseFlow).init()
-
+fun ComputerComponent() {
     Column {
-        Text(text = Translation.Game.COMPUTER)
-
-        Button(onClick = { userInputFlow.tryEmit("123") }) {
-            Text(text = "Emit")
-        }
-
-        Text(text = "State:")
-
+        Header(title = Translation.Game.COMPUTER)
     }
-
 
     // TODO GAME-54
 }
