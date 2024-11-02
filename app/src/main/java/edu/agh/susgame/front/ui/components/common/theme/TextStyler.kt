ea338ea44c9f6bbcea4f571ecf0686e6f6a14095
uqsz <uqsz123@gmail.com>
2024-11-02 15:51:07 +0100

    TextStyler update

--- app/src/main/java/edu/agh/susgame/front/ui/components/common/theme/TextStyler.kt
+++ app/src/main/java/edu/agh/susgame/front/ui/components/common/theme/TextStyler.kt
@@ -6,35 +6,25 @@ import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 
 object TextStyler {
-    val TerminalLarge = TextStyle(
-        color = Color.Gray,
-        fontSize = 14.sp,
-        fontFamily = FontFamily.Monospace,
-        fontWeight = FontWeight.Bold,          // Grubość czcionki, np. Normal, Medium, Bold
-        letterSpacing = 1.5.sp,                // Odstęp między literami
-//        textAlign = TextAlign.Center,          // Wyrównanie tekstu, np. Left, Center, Right, Justify
-        shadow = Shadow(                       // Cień tekstu
-            color = Color.Black,
-            offset = Offset(2f, 2f),
-            blurRadius = 4f
-        ),
-    )
-    val TerminalMedium = TextStyle(
-        color = Color.Gray,
-        fontSize = 11.sp,
-        fontFamily = FontFamily.Monospace,
-        fontWeight = FontWeight.Bold,          // Grubość czcionki, np. Normal, Medium, Bold
-        letterSpacing = 1.5.sp,                // Odstęp między literami
-//        textAlign = TextAlign.Center,          // Wyrównanie tekstu, np. Left, Center, Right, Justify
-        shadow = Shadow(                       // Cień tekstu
-            color = Color.Black,
-            offset = Offset(2f, 2f),
-            blurRadius = 4f
-        ),
-    )
+    private fun baseTextStyle(fontSize: TextUnit): TextStyle {
+        return TextStyle(
+            color = Color.Gray,
+            fontSize = fontSize,
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.Bold,
+            letterSpacing = 1.5.sp,
+            shadow = Shadow(
+                color = Color.Black,
+                offset = Offset(2f, 2f),
+                blurRadius = 4f
+            )
+        )
+    }
 
-
+    val TerminalLarge = baseTextStyle(14.sp)
+    val TerminalMedium = baseTextStyle(11.sp)
 }
