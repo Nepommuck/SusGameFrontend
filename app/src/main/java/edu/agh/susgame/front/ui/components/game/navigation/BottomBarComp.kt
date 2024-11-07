00ba9d53cf4487b2978debc406d6c18bfd582c21
uqsz <uqsz123@gmail.com>
2024-11-07 01:00:33 +0100

    deleting files

--- app/src/main/java/edu/agh/susgame/front/ui/components/game/navigation/BottomBarComp.kt
+++ app/src/main/java/edu/agh/susgame/front/ui/components/game/navigation/BottomBarComp.kt
@@ -1,52 +1,53 @@
 package edu.agh.susgame.front.ui.components.game.navigation
 
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.navigation.NavHostController
-import edu.agh.susgame.front.ui.components.common.util.BottomNavigationItem
+//import androidx.compose.material3.Icon
+//import androidx.compose.material3.NavigationBar
+//import androidx.compose.material3.NavigationBarItem
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.mutableIntStateOf
+//import androidx.compose.runtime.remember
+//import androidx.compose.runtime.setValue
+//import androidx.navigation.NavHostController
+//import edu.agh.susgame.front.ui.components.common.util.BottomNavigationItem
 
-@Composable
-fun BottomBarComponent(navController: NavHostController) {
-    var navigationSelectedItem by remember {
-        mutableIntStateOf(0)
-    }
-
-    NavigationBar {
-        BottomNavigationItem.allMenuItems
-            .forEachIndexed { index, navigationItem ->
-                NavigationBarItem(
-                    selected = index == navigationSelectedItem,
-                    label = {
-                        Text(navigationItem.label)
-                    },
-                    icon = {
-                        Icon(
-                            navigationItem.icon,
-                            contentDescription = navigationItem.label,
-                        )
-                    },
-                    onClick = {
-                        navigationSelectedItem = index
-                        navController.navigate(navigationItem.route)
-                        //    TODO Those lines were taken from an internet example and are
-                        //     designed to somehow keep the state. We may need them in the
-                        //     future, but for now they were doing more harm
-                        //  {
-                        //    popUpTo(navController.graph.findStartDestination().id) {
-                        //        saveState = true
-                        //    }
-                        //    launchSingleTop = true
-                        //    restoreState = true
-                        //  }
-                    }
-                )
-            }
-    }
-}
+// IT CAN BE DELETED
+//@Composable
+//fun BottomBarComponent(navController: NavHostController) {
+//    var navigationSelectedItem by remember {
+//        mutableIntStateOf(0)
+//    }
+//
+//    NavigationBar {
+//        BottomNavigationItem.allMenuItems
+//            .forEachIndexed { index, navigationItem ->
+//                NavigationBarItem(
+//                    selected = index == navigationSelectedItem,
+//                    label = {
+//                        Text(navigationItem.label)
+//                    },
+//                    icon = {
+//                        Icon(
+//                            navigationItem.icon,
+//                            contentDescription = navigationItem.label,
+//                        )
+//                    },
+//                    onClick = {
+//                        navigationSelectedItem = index
+//                        navController.navigate(navigationItem.route)
+//                        //    TODO Those lines were taken from an internet example and are
+//                        //     designed to somehow keep the state. We may need them in the
+//                        //     future, but for now they were doing more harm
+//                        //  {
+//                        //    popUpTo(navController.graph.findStartDestination().id) {
+//                        //        saveState = true
+//                        //    }
+//                        //    launchSingleTop = true
+//                        //    restoreState = true
+//                        //  }
+//                    }
+//                )
+//            }
+//    }
+//}
