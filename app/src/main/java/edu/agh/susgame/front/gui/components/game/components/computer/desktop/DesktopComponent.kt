69d064b15160c5d5bac9d164788ee9cb257fb4c0
Nepommuck <pawel.konopka5@gmail.com>
2024-11-28 20:05:09 +0100

    Windows 98 vibes

--- app/src/main/java/edu/agh/susgame/front/gui/components/game/components/computer/desktop/DesktopComponent.kt
+++ app/src/main/java/edu/agh/susgame/front/gui/components/game/components/computer/desktop/DesktopComponent.kt
@@ -1,5 +1,6 @@
 package edu.agh.susgame.front.gui.components.game.components.computer.desktop
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
@@ -8,17 +9,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun DesktopComponent() {
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Blue)
+    ) {
         Button(
             onClick = {},
             modifier = Modifier
                 .align(Alignment.TopStart)
-                .offset(x = 50.dp, y = 50.dp)
+                .offset(x = 50.dp, y = 50.dp),
         ) {
             Text("Icon-1")
         }
